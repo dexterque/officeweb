b06bfab86ad78d28e55272adb92aa20973aaffa0
dexterque <dexterque@qq.com>
2018-05-14 13:07:00 +0800

    add

---
+++ storage/cms/cache/56/1e/category.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms5af71af6e66c7390170555_183eacd4ea9e9376f170db9547e88c00Class extends \Cms\Classes\PageCode
+{
+
+}
---
+++ storage/cms/twig/26/26198103ced98f8ced28e48450bd92a838073cedc9d738f349778adc339dfbbd.php
@@ -0,0 +1,36 @@
+<?php
+
+/* D:\phpStudy\PHPTutorial\WWW\officeweb/themes/ahmadcholluyev-mogo-theme/pages/post.htm */
+class __TwigTemplate_1f4324b8b1cc0952a53397f50dfbf70a3663495ef75e2cea7fddf048ffc9b12c extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "XX";
+    }
+
+    public function getTemplateName()
+    {
+        return "D:\\phpStudy\\PHPTutorial\\WWW\\officeweb/themes/ahmadcholluyev-mogo-theme/pages/post.htm";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("XX", "D:\\phpStudy\\PHPTutorial\\WWW\\officeweb/themes/ahmadcholluyev-mogo-theme/pages/post.htm", "");
+    }
+}
